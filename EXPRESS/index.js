74f96c432d6bafd962ed0c4aed9c140958cc48dc
Tiago de Souza Moro Conke <122630395+TigasConke@users.noreply.github.com>
2024-08-15 17:15:28 -0300

    Add files via upload

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -3,6 +3,24 @@ const app = express(); //iniciando o express
 
 app.listen(300, function(erro){ //chamando o servidor, 300 é a porta
 
+    app.get("/",function(req,res){
+
+        res.send("Salve meu querido!");
+
+    });
+
+    app.get("/perfil",function(req,res){
+
+        res.send("Esse é o seu perfil.");
+
+    });
+
+    app.get("/pedido/pagamento", function(req, res){
+
+        res.send("Realize o pagamento!");
+
+    })
+
     if(erro){
         
         console.log("Ocorreu um erro!");
