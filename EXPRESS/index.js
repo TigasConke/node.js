8012e543fd8ca90ccb173d7b2d2055ac6fc08de3
Tiago de Souza Moro Conke <122630395+TigasConke@users.noreply.github.com>
2024-09-11 12:51:31 -0300

    Add files via upload

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -1,11 +1,11 @@
 const express = require("express"); //importando o express
 const app = express(); //iniciando o express
 
-app.listen(300, function(erro){ //chamando o servidor, 300 é a porta
+app.listen(300, function(erro){ // chamando o servidor, 300 é a porta
 
     app.get("/",function(req,res){
 
-        res.send("Salve meu querido!");
+        res.send("Salve, Salve!");
 
     });
 
@@ -18,6 +18,33 @@ app.listen(300, function(erro){ //chamando o servidor, 300 é a porta
     app.get("/pedido/pagamento", function(req, res){
 
         res.send("Realize o pagamento!");
+
+    })
+
+    app.get("/ola/:nome/:empresa", function(req,res){ // :nome e :empresa é um parâmetro da rota
+
+        var empresa = req.params.empresa;
+        var nome = req.params.nome;
+        res.send("<h1> Oi meu chapa " + nome + " você está em " + empresa + "</h1>");
+
+    });
+
+    app.get("/sua/:religiao?", function(req, res){
+
+        var religiao = req.params.religiao;
+
+        if(religiao === undefined){
+
+            res.send("<h1> Você não segue nenhuma religião </h1>");
+
+        }
+
+        else{
+
+            res.send("<h1> Sua religião é " + religiao + "</h1>");
+
+        }
+        
 
     })
 
